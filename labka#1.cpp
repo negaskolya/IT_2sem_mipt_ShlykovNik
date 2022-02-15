ff184cfe0b7fba434518fece4cdb5d7108bccd08
Nikolay Shlykov <ShlykovNikolaj@yandex.ru>
2022-02-15 10:35:51 +0300

    Uploading file programs

---
+++ labka#1.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Toshiba on 15.02.2022.
+//
+
---
+++ reversing_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int reverser(int &number, int &k)
+{
+    int i = 0, rest = 0;
+    for(i = 0; i < n; i++) {
+        rest = number % 10;
+        number = number / 10;
+        number = number + rest *
+    }
+}
+
+int main() {
+    int num, n;
+    std::cin >> num >> n;
+    print("%d", reverser(num, n));
+}
